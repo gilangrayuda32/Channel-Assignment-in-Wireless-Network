5bce4edda41c9271a31ef4b58448968831373ed3
gilangrayuda32 <113423105+gilangrayuda32@users.noreply.github.com>
2023-08-29 14:33:36 +0900

    distance.m

--- distance.m
+++ distance.m
@@ -1,23 +1,24 @@
 function [dist, min2maxdist, idxDist1] = distance(coorT, coorR)
 global neighbor;
-    indexX = 1;     % Index untuk transmitter
-    indexY = 2;     % Index untuk Receiver
+    indexX = 1;     % Index for transmitter
+    indexY = 2;     % Index for Receiver
     
     for c = 1:length(coorT)
-        % Calculate distance beetween two coordinate
+        % Calculate distance beetween two nodes
         for z=1:length(coorT)
              dist(c,z) = sqrt((coorR(c,indexX)-coorT(z,indexX))^2+ (coorR(c,indexY)-coorT(z,indexY))^2); 
         end 
     end
     
+%% Note: The subsequent codes are utilized for extended simulation, which is neglected for this simulation.
     zerodist = dist;
     for i=1:length(coorT)
         zerodist(i,i) = 0;
     end
-    min2maxdist = sort(zerodist,2);             % Sudah diurutkan
-    min2maxdist = exclude(min2maxdist,1,'col');  % Menghilangkan 0 pada firs kolom
+    min2maxdist = sort(zerodist,2);                  % Sorting Distance
+    min2maxdist = exclude(min2maxdist,1,'col');      % Eliminate 0
     
-    %%---------->>Menentukan index 5 distence terdekat<<---------------%%
+    %%---------->>Find nearest neighbor<<---------------%%
     urutIte = (neighbor*2);
     for i=1: length(coorT)
         for j=1:urutIte
