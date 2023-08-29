f98b12da6a3ff83bbbca283f2754406b4a629d95
gilangrayuda32 <113423105+gilangrayuda32@users.noreply.github.com>
2023-08-29 14:32:24 +0900

    channelgainnew.m

--- channelgainnew.m
+++ channelgainnew.m
@@ -1,12 +1,16 @@
 function [G, min2maxG] = channelgainnew(dist, lengthCoorT)
 global a dr;
-  c = 3*10^8;             % Light Speed
+
+%% Indoor Fading Channel
+    c = 3*10^8;             % Light Speed
     f = 5*10^9;             % 5 GHz
     l = c / f;
-    g =20;                  % saya ubah menjadi ini
+    InitGain =20;             
     K = 10^(log10(l/(4*3.14*dr)));
-    G = g* K * ((dr./dist).^a);
+    G = InitGain* K * ((dr./dist).^a);
     
+
+%% Note: The subsequent codes are utilized for extended simulation, which is neglected for this simulation.
     %%---------->> Sum Rate Each pairs <<----------%%%
     zeroCG = G;
     for i=1:lengthCoorT    
@@ -15,8 +19,8 @@ global a dr;
     min2maxG = sort(zeroCG,2);
     min2maxG = exclude(min2maxG,1,'col');  % Menghilangkan 0 pada firs kolom
     
-    %%---------->> Sum Rate Each pairs <<----------%%%
-%      %%---------->>Menentukan index 5 distence terdekat<<---------------%%
+%%---------->> Sum Rate Each pairs <<----------%%
+%      %%---------->>Finding 5 nearest nerighbor<<---------------%%
 %     urutIte = (neighbor*2);
 %     for i=1: length(coorT)
 %         for j=1:urutIte
